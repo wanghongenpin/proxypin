52bb722cbb9ab12767951f7d45f2d026362249e6
twhps105005 <twhps105005@gmail.com>
2025-12-18 09:09:50 +0000

    Fix contrast issue in dark mode in theme.dart (Issue #656)

--- lib/ui/component/json/theme.dart
+++ lib/ui/component/json/theme.dart
@@ -12,13 +12,20 @@ class ColorTheme {
         searchMatchCurrentColor: colorScheme.primary,
       );
 
+  static Color _lighten(Color color, [double amount = 0.2]) {
+        final hsl = HSLColor.fromColor(color);
+        final hslLight =
+            hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+        return hslLight.toColor();
+  }
+
   static ColorTheme dark(ColorScheme colorScheme) => ColorTheme(
         background: const Color(0xff2b2b2b),
-        propertyKey: const Color(0xff9876aa),
-        colon: const Color(0xffcc7832),
-        string: const Color(0xff6a8759),
-        number: const Color(0xff6897bb),
-        keyword: const Color(0xffcc7832),
+        propertyKey: _lighten(const Color(0xff9876aa), 0.18),
+        colon: _lighten(const Color(0xffcc7832), 0.15),
+        string: _lighten(const Color(0xff6a8759), 0.20),
+        number: _lighten(const Color(0xff6897bb), 0.20),
+        keyword: _lighten(const Color.fromRGBO(204, 120, 50, 1), 0.15),
         searchMatchColor: colorScheme.inversePrimary,
         searchMatchCurrentColor: colorScheme.primary,
       );
