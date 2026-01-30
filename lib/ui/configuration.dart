02bc02880c19473da7d725ab3b111b73b9939b63
wanghongenpin <wanghongenpin@gmail.com>
2026-01-30 18:11:16 +0800

    v1.2.5

--- lib/ui/configuration.dart
+++ lib/ui/configuration.dart
@@ -63,7 +63,7 @@ class ThemeModel {
 }
 
 class AppConfiguration {
-  static const String version = "1.2.4";
+  static const String version = "1.2.5";
 
   ValueNotifier<bool> globalChange = ValueNotifier(false);
 
@@ -71,7 +71,7 @@ class AppConfiguration {
   Locale? _language;
 
   //是否显示更新内容公告
-  bool upgradeNoticeV24 = true;
+  bool upgradeNoticeV25 = true;
 
   /// 是否启用画中画
   ValueNotifier<bool> pipEnabled = ValueNotifier(Platform.isAndroid);
@@ -199,7 +199,7 @@ class AppConfiguration {
       _theme = ThemeModel(mode: mode, useMaterial3: config['useMaterial3'] ?? true);
       _theme.color = config['themeColor'] ?? "Blue";
 
-      upgradeNoticeV24 = config['upgradeNoticeV24'] ?? true;
+      upgradeNoticeV25 = config['upgradeNoticeV25'] ?? true;
       _language = config['language'] == null 
         ? null 
         : Locale.fromSubtags(
@@ -251,7 +251,7 @@ class AppConfiguration {
       'mode': _theme.mode.name,
       'themeColor': _theme.color,
       'useMaterial3': _theme.useMaterial3,
-      'upgradeNoticeV24': upgradeNoticeV24,
+      'upgradeNoticeV25': upgradeNoticeV25,
       "language": _language?.languageCode,
       "languageScript": _language?.scriptCode,
       "headerExpanded": headerExpanded,
